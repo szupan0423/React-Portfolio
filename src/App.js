9a2fc75939fb6d6c5bd6eeacd3ee074c56da8797
szupan0423 <111537299+szupan0423@users.noreply.github.com>
2023-02-01 23:45:18 -0600

    added more js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Navigation from './components/Navigation';
+import { BrowserRouter, Router,Route } from 'react-router-dom';
+import About from './components/About';
 
 function App() {
   return (
-    
+    <BrowserRouter basename='react-portfolio'>
+      <Header />
+      <Routes>
+        <Route path='/'element={<About />} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+//import img of me
+
+function About() {
+    return(
+        <div id='about-me'>
+            <div id='bio'>
+                <p> fill in later </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
